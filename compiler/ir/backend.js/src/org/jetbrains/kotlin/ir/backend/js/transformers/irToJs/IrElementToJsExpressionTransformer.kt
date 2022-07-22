16049ccc5ec574ec7b40418d96de3ff4c3ae6b38
Artem Kobzar <artem.kobzar@jetbrains.com>
2022-07-22 20:22:27 +0200

    chore: save week work on interfaces.

--- compiler/ir/backend.js/src/org/jetbrains/kotlin/ir/backend/js/transformers/irToJs/IrElementToJsExpressionTransformer.kt
+++ compiler/ir/backend.js/src/org/jetbrains/kotlin/ir/backend/js/transformers/irToJs/IrElementToJsExpressionTransformer.kt
@@ -128,10 +128,15 @@ class IrElementToJsExpressionTransformer : BaseIrElementToJsNodeTransformer<JsEx
 
     override fun visitGetObjectValue(expression: IrGetObjectValue, context: JsGenerationContext): JsExpression {
         val obj = expression.symbol.owner
-        assert(obj.kind == ClassKind.OBJECT)
-        assert(obj.isEffectivelyExternal()) { "Non external IrGetObjectValue must be lowered" }
 
-        return context.getRefForExternalClass(obj).withSource(expression, context)
+//        assert(obj.kind == ClassKind.OBJECT)
+//        assert(obj.isEffectivelyExternal()) { "Non external IrGetObjectValue must be lowered" }
+
+        return if (obj.isEffectivelyExternal()) {
+            context.getRefForExternalClass(obj)
+        } else {
+            context.getNameForClass(obj).makeRef()
+        }.withSource(expression, context)
     }
 
     override fun visitSetField(expression: IrSetField, context: JsGenerationContext): JsExpression {
